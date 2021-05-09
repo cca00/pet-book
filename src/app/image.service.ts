52baf6fa3ce07661725c0fa7863525dbf2a772e7
Cristhian Chica <cc.acosta00@gmail.com>
2021-05-09 18:51:44 -0500

    more fixes

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@angular/core'    
-@Injectable()    
 export class ImageService {    
     allImages = [];    
     
     getImages() {    
-        return this.allImages = Imagesdelatils.slice(0);    
+        this.allImages = Imagesdelatils.slice(0);    
+        return this.allImages;
     }    
     
     getImage(id: number) {    
